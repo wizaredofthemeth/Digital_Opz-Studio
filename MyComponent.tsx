8c1223df49313aebfee59f245dbb1df68db7eece
wizaredofthemeth <wizaredofthemeth@gmail.com>
2025-08-07 11:05:19 -0700

    Create MyComponent.tsx

---
+++ MyComponent.tsx
@@ -0,0 +1,12 @@
+import MyComponent from "MyComponent";
+
+function App() {
+  return (
+    <div>
+      <MyComponent />
+      {/* ...other components */}
+    </div>
+  );
+}
+
+export default App;
